717c3b133328f3dd326856caa8ce360093da3401
Colt Skeen <93236823+coltskeen@users.noreply.github.com>
2022-02-03 22:16:38 -0500

    9x9 Checkerboard: Black and Red Squares

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Your JS goes here
+for (var i = 1; i < 64; i++) {
+    const div = document.createElement('div');
+    div.style.width = '11.1%';
+    div.style.float = 'left';
+    div.style.paddingBottom = '11.1%';
+    const squareColor = (i % 2 === 0) ? (div.style.backgroundColor = "red") : (div.style.backgroundColor = "black"); 
+    document.body.append(div);
+}
