ee8fd3a56c7a03aa8ef126bdc7680b34cae585cf
Colt Skeen <93236823+coltskeen@users.noreply.github.com>
2022-02-06 18:05:17 -0500

    base commit before edits for gradient

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 // Your JS goes here
-for (var i = 1; i < 64; i++) {
-    const div = document.createElement('div');
-    div.style.width = '11.1%';
-    div.style.float = 'left';
-    div.style.paddingBottom = '11.1%';
-    const squareColor = (i % 2 === 0) ? (div.style.backgroundColor = "red") : (div.style.backgroundColor = "black"); 
-    document.body.append(div);
+
+
+function colorTiles() {
+    for (var i = 1; i < 9; i++) {
+        for (var j = 1; j < 9; j++) {
+            let r = Math.floor(Math.random() * 255);
+            console.log(r);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            const div = document.createElement('div');
+            div.style.height = '11.1%';
+            div.style.width = '11.1%';
+            div.style.float = 'left';
+            div.style.paddingBottom = '11.1%';
+            if (i % 2 === 0) {
+                if (j % 2 === 0) {
+                    div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"; 
+                } else {
+                    div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+                }
+            } else {
+                if (j % 2 === 0) {
+                    div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+                } else {
+                    div.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+                }
+            }
+            document.body.appendChild(div);
+        }
+    }
+    console.log('I did it!');
 }
 
-//some text for a test 
+colorTiles();
